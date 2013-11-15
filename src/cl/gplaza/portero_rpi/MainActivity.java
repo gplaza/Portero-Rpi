152e41092a06c98e92b748f520251cce3818c3c9
Administrateur <Administrateur@Android-dev.lan>
2013-11-15 03:03:43 -0300

    add simple call web service REST

--- src/cl/gplaza/portero_rpi/MainActivity.java
+++ src/cl/gplaza/portero_rpi/MainActivity.java
@@ -1,15 +1,24 @@
 package cl.gplaza.portero_rpi;
 
+import cl.gplaza.portero_rpi.RestClient.RequestMethod;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+
+	Button buttonOpen;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
+		addListenerOnButton();
 	}
 
 	@Override
@@ -17,6 +26,29 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void addListenerOnButton() {
+
+		buttonOpen = (Button) findViewById(R.id.button1);
+
+		buttonOpen.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+
+				RestClient client = new RestClient("http://192.168.0.28:8080/hello/gilles");
+				
+				try {
+				    client.Execute(RequestMethod.GET);
+				} catch (Exception e) {
+				    e.printStackTrace();
+				}
+
+				String response = client.getResponse();
+				Log.d("cl.gplaza.portero_rpi", response);
+			}
+		});
 	}
 
 }
