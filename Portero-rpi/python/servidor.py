ca5f116a708c7690b36c0562d7ad8edfbdb88256
Administrateur <Administrateur@Android-dev.lan>
2013-11-16 15:07:41 -0300

    delete doublon python

--- Portero-rpi/python/servidor.py
+++
@@ -1,7 +0,0 @@
-from bottle import route, run, template
-
-@route('/door/<name>')
-def index(name='World'):
-    return template('<b>Hello {{name}}</b>!', name=name)
-
-run(host='192.168.0.28', port=8080)
