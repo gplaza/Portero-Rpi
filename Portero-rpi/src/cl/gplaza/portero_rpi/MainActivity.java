842775583d54f654a61a7d54f8d147716d10655b
Administrateur <Administrateur@Android-dev.lan>
2013-11-16 14:34:05 -0300

    cleaning

--- Portero-rpi/src/cl/gplaza/portero_rpi/MainActivity.java
+++ Portero-rpi/src/cl/gplaza/portero_rpi/MainActivity.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
-import android.util.Log;
 
 public class MainActivity extends Activity {
 
